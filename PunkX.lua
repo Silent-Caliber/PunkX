5a8418d1d33d0b438b2d55499ea885ebdabbf733
Silent-Caliber <146331513+Silent-Caliber@users.noreply.github.com>
2025-12-07 23:25:53 +0800

    Update PunkX.lua

--- PunkX.lua
+++ PunkX.lua
@@ -1,6 +1,6 @@
 {
   "moduleName": "PunkX",
-  "script_url": "https://raw.githubusercontent.com/DmelScripts/luau/refs/heads/main/Place_118399039039526_Script_1765087863.txt",
+  "script_url": "https://raw.githubusercontent.com/DmelScripts/luau/refs/heads/main/Roblox%20project%20Israel",
   "disc_invite": "https://discord.gg/JxEjAtdgWD",
   "versionName": "Android"
 }

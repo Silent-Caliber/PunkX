69f190ee0a3f342a3c8b4f1ed83de7077ffa9fc3
Silent-Caliber <146331513+Silent-Caliber@users.noreply.github.com>
2025-12-07 16:15:52 +0800

    Add initial module configuration for PunkX

---
+++ PunkX.lua
@@ -0,0 +1,6 @@
+{
+  "moduleName": "PunkX",
+  "script_url": "https://raw.githubusercontent.com/DmelScripts/luau/refs/heads/main/c958b2e48daf49de.lua.txt",
+  "disc_invite": "https://discord.gg/JxEjAtdgWD",
+  "versionName": "Android"
+}

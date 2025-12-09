bf9effc859651928ec3e049d6219d0a05b8a7270
Silent-Caliber <146331513+Silent-Caliber@users.noreply.github.com>
2025-12-09 23:44:51 +0800

    Rename module from PunkX to Seliware

--- PunkX.lua
+++ PunkX.lua
@@ -1,5 +1,5 @@
 {
-  "moduleName": "PunkX",
+  "moduleName": "Seliware",
   "script_url": "https://raw.githubusercontent.com/DmelScripts/luau/refs/heads/main/Roblox%20project%20Israel",
   "disc_invite": "https://discord.gg/JxEjAtdgWD",
   "versionName": "Android"

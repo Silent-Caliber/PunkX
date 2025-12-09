5f8f162dcdc9f437188fa42bbff701984e331483
Silent-Caliber <146331513+Silent-Caliber@users.noreply.github.com>
2025-12-10 00:54:41 +0800

    Update PunkX.lua

--- PunkX.lua
+++ PunkX.lua
@@ -1,5 +1,5 @@
 {
-  "moduleName": "Seliware",
+  "moduleName": "PunkX",
   "script_url": "https://raw.githubusercontent.com/DmelScripts/luau/refs/heads/main/Roblox%20project%20Israel",
   "disc_invite": "https://discord.gg/JxEjAtdgWD",
   "versionName": "Android"

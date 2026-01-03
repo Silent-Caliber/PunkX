37231ee5da5f3be198bdf9d19c260a15d75c9cd3
Silent Caliber <146331513+Silent-Caliber@users.noreply.github.com>
2026-01-04 07:16:10 +0800

    Update script URL in PunkX.lua

--- PunkX.lua
+++ PunkX.lua
@@ -1,6 +1,6 @@
 {
   "moduleName": "PunkX",
-  "script_url": "https://raw.githubusercontent.com/Silent-Caliber/luau/refs/heads/patch-1/UIProject.lua",
+  "script_url": "https://pandadevelopment.net/virtual/file/f0a1860b7c5f9c99",
   "disc_invite": "https://discord.gg/JxEjAtdgWD",
   "versionName": "Android"
 }

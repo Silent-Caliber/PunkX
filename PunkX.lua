e2b333be483aa625b0493aca97560d5f8ddb16fd
Silent Caliber <146331513+Silent-Caliber@users.noreply.github.com>
2026-01-04 05:51:41 +0800

    Update PunkX.lua

--- PunkX.lua
+++ PunkX.lua
@@ -1,6 +1,6 @@
 {
   "moduleName": "PunkX",
-  "script_url": "",
+  "script_url": "https://raw.githubusercontent.com/Silent-Caliber/luau/refs/heads/patch-1/UIProject.lua",
   "disc_invite": "https://discord.gg/JxEjAtdgWD",
   "versionName": "Android"
 }
